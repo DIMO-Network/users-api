44db70bdcb9d9e6dd8e87613cf90fb85767cebcc
Dylan Moreland <dylan@dimo.zone>
2021-11-30 14:59:21 -0500

    Sketch out DB

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.17
 require (
 	github.com/gofiber/fiber/v2 v2.22.0
 	github.com/gofiber/jwt/v3 v3.2.1
+	github.com/golang-jwt/jwt/v4 v4.1.0
+	github.com/mattn/go-sqlite3 v1.14.9
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
-	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
 	github.com/klauspost/compress v1.13.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"database/sql"
+
 	jwtware "github.com/gofiber/jwt/v3"
 	"github.com/golang-jwt/jwt/v4"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const jwksURL = "http://127.0.0.1:5556/dex/keys"
+
+type User struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+}
 
 func checkUser(c *fiber.Ctx) error {
 	user := c.Locals("user").(*jwt.Token)
@@ -21,18 +29,42 @@ func checkUser(c *fiber.Ctx) error {
 	return c.Next()
 }
 
-func getUser(c *fiber.Ctx) error {
-	c.SendString("Good work, " + c.Params("userID"))
-	return nil
+func getOrCreateUser(userID string) User {
+	var email string
+	if err := db.QueryRow("SELECT email FROM users WHERE id = ?;", userID).Scan(&email); err != nil {
+		db.Exec("INSERT INTO users (sub) VALUES (?);", userID)
+	}
+	return User{Id: userID, Email: email}
 }
 
+func getUserHandler(c *fiber.Ctx) error {
+	userID := c.Params("userID")
+	user := getOrCreateUser(userID)
+	return c.JSON(user)
+}
+
+func updateUserHandler(c *fiber.Ctx) error {
+	userID := c.Params("userID")
+	user := getOrCreateUser(userID)
+	// Going to pick up dog
+}
+
+var db *sql.DB
+
 func main() {
+	db, _ = sql.Open("sqlite3", ":memory:")
+
+	defer db.Close()
+
+	db.Exec("CREATE TABLE users (id TEXT PRIMARY KEY, email TEXT);")
+
 	app := fiber.New()
 
 	jwtMiddle := jwtware.New(jwtware.Config{KeySetURL: jwksURL})
 
 	v1 := app.Group("/v1/users/:userID", jwtMiddle, checkUser)
-	v1.Get("/", getUser)
+	v1.Get("/", getUserHandler)
+	v1.Put("/", updateUserHandler)
 
 	log.Fatal(app.Listen(":3000"))
 }

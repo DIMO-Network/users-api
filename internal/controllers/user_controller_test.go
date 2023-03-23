000d5bce201dbcb83b592290ec34f41ddaf0f487
Doyin.O <111298305+0xdev22@users.noreply.github.com>
2023-03-23 04:50:26 -0400

    Update internal/controllers/user_controller_test.go
    Co-authored-by: Dylan Moreland <79415431+elffjs@users.noreply.github.com>

--- internal/controllers/user_controller_test.go
+++ internal/controllers/user_controller_test.go
@@ -350,7 +350,7 @@ func (s *UserControllerTestSuite) TestNoReferralCodeWithoutEthereumAddress() {
 	err = json.NewDecoder(resp.Body).Decode(&user)
 	s.Require().NoError(err)
 
-	s.Empty(user.ReferralCode.String)
+	s.False(user.ReferralCode.Valid)
 }
 
 func (s *UserControllerTestSuite) TestReferralCodeGeneratedOnWeb3Provider() {

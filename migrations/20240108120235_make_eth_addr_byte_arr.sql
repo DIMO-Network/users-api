bfbaaf19619d92ca816a69160818c4d2a1151d8d
Eduardo Rodriguez <eduardorodriguez@eduardos-macbook-pro.home>
2024-01-09 00:49:46 +0100

    [SM-403] fix typo

--- migrations/20240108120235_make_eth_addr_byte_arr.sql
+++ migrations/20240108120235_make_eth_addr_byte_arr.sql
@@ -10,13 +10,13 @@ INTO    users_api.users_eth
 FROM    users_api.users;
 
 ALTER TABLE users_api.users
-    ALTER COLUMN ehtereum_address TYPE bytea 
+    ALTER COLUMN ethereum_address TYPE bytea 
     USING NULL;
 
 LOCK TABLE users_api.users IN ACCESS EXCLUSIVE MODE;
 
 UPDATE users_api.users
-SET ehtereum_address = decode(substr(eth, 3), 'hex')
+SET ethereum_address = decode(substr(eth, 3), 'hex')
 FROM users_api.users_eth
 WHERE users.id = users_eth.id;
 
@@ -32,18 +32,18 @@ SET search_path TO users_api, public;
 BEGIN TRANSACTION;
 
 SELECT  id,
-        ehtereum_address as eth
+        ethereum_address as eth
 INTO    users_api.users_eth
 FROM    users_api.users;
 
 ALTER TABLE users_api.users
-    ALTER COLUMN ehtereum_address TYPE TEXT 
+    ALTER COLUMN ethereum_address TYPE TEXT 
     USING NULL;
 
 LOCK TABLE users_api.users IN ACCESS EXCLUSIVE MODE;
 
 UPDATE users_api.users
-SET ehtereum_address = '0x' || encode(eth, 'hex')
+SET ethereum_address = '0x' || encode(eth, 'hex')
 FROM users_api.users_eth
 WHERE users.id = users_eth.id;
 

c01d2c5aee15b68556e7af6e880a51a9a3af58f1
Eduardo Rodriguez <eduardorodriguez@eduardos-macbook-pro.home>
2024-01-09 00:34:54 +0100

    [SM-403] Add eth address migration script

---
+++ migrations/20240108120235_make_eth_addr_byte_arr.sql
@@ -0,0 +1,56 @@
+-- +goose Up
+-- +goose StatementBegin
+SET search_path TO users_api, public;
+
+BEGIN TRANSACTION;
+
+SELECT  id,
+        ehtereum_address as eth
+INTO    users_api.users_eth
+FROM    users_api.users;
+
+ALTER TABLE users_api.users
+    ALTER COLUMN ehtereum_address TYPE bytea 
+    USING NULL;
+
+LOCK TABLE users_api.users IN ACCESS EXCLUSIVE MODE;
+
+UPDATE users_api.users
+SET ehtereum_address = decode(substr(eth, 3), 'hex')
+FROM users_api.users_eth
+WHERE users.id = users_eth.id;
+
+ALTER TABLE users_api.users
+    ALTER COLUMN ehtereum_address SET NOT NULL;
+
+DROP TABLE users_api.users_eth;
+
+COMMIT TRANSACTION;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SET search_path TO users_api, public;
+
+BEGIN TRANSACTION;
+
+SELECT  id,
+        ehtereum_address as eth
+INTO    users_api.users_eth
+FROM    users_api.users;
+
+ALTER TABLE users_api.users
+    ALTER COLUMN ehtereum_address TYPE TEXT 
+    USING NULL;
+
+LOCK TABLE users_api.users IN ACCESS EXCLUSIVE MODE;
+
+UPDATE users_api.users
+SET ehtereum_address = '0x' || encode(eth, 'hex')
+FROM users_api.users_eth
+WHERE users.id = users_eth.id;
+
+DROP TABLE users_api.users_eth;
+
+COMMIT TRANSACTION;
+-- +goose StatementEnd

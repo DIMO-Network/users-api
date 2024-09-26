46fa9a33897b742033f0921c1c526f41b4b740ab
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2024-09-26 15:32:20 -0400

    List wallet stats on /check-email (#110)

--- internal/controllers/admin_handlers.go
+++ internal/controllers/admin_handlers.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"math/big"
+
+	"github.com/DIMO-Network/users-api/internal/controllers/contracts"
 	"github.com/DIMO-Network/users-api/models"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gofiber/fiber/v2"
 	"github.com/volatiletech/null/v8"
 )
@@ -21,23 +26,94 @@ func (d *UserController) CheckEmail(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Couldn't parse body.")
 	}
 
-	exists, err := models.Users(
+	users, err := models.Users(
 		models.UserWhere.EmailAddress.EQ(null.StringFrom(cer.Address)),
 		models.UserWhere.EmailConfirmed.EQ(true),
-	).Exists(c.Context(), d.dbs.DBS().Reader)
+		models.UserWhere.EthereumConfirmed.EQ(true),
+	).All(c.Context(), d.dbs.DBS().Reader)
 	if err != nil {
 		return err
 	}
 
-	return c.JSON(CheckEmailResponse{InUse: exists})
+	usedInApp, usedExternal := 0, 0
+
+	client, err := ethclient.Dial(d.Settings.MainRPCURL)
+	if err != nil {
+		return err
+	}
+
+	ad, _ := contracts.NewMultiPrivilege(common.HexToAddress(d.Settings.ADNFTAddr), client)
+	v, _ := contracts.NewMultiPrivilege(common.HexToAddress(d.Settings.VehicleNFTAddr), client)
+	tk, _ := contracts.NewToken(common.HexToAddress(d.Settings.TokenAddr), client)
+
+	for _, user := range users {
+		if !user.EthereumAddress.Valid || len(user.EthereumAddress.Bytes) != common.AddressLength {
+			d.log.Warn().Msg("User %s is marked as having a confirmed Ethereum address, but the address is invalid.")
+			continue
+		}
+
+		addr := common.BytesToAddress(user.EthereumAddress.Bytes)
+
+		zero := big.NewInt(0)
+
+		used, err := func() (bool, error) {
+			if adBal, err := ad.BalanceOf(nil, addr); err != nil {
+				return false, err
+			} else if adBal.Cmp(zero) > 0 {
+				return true, nil
+			}
+
+			if vBal, err := v.BalanceOf(nil, addr); err != nil {
+				return false, err
+			} else if vBal.Cmp(zero) > 0 {
+				return true, nil
+			}
+
+			if tkBal, err := tk.BalanceOf(nil, addr); err != nil {
+				return false, err
+			} else if tkBal.Cmp(zero) > 0 {
+				return true, nil
+			}
+
+			return false, nil
+		}()
+		if err != nil {
+			return err
+		}
+		if !used {
+			continue
+		}
+
+		if user.InAppWallet {
+			usedInApp++
+		} else {
+			usedExternal++
+		}
+	}
+
+	return c.JSON(CheckEmailResponse{
+		InUse: usedInApp > 0 || usedExternal > 0,
+		Wallets: struct {
+			External int `json:"external"`
+			InApp    int `json:"inApp"`
+		}{
+			External: usedExternal,
+			InApp:    usedInApp,
+		},
+	})
 }
 
 type CheckEmailRequest struct {
 	// Address is the email address to check. Must be confirmed.
-	Address string `json:"address"`
+	Address string `json:"address" example:"thaler@a16z.com"`
 }
 
 type CheckEmailResponse struct {
 	// InUse specifies whether the email is attached to a DIMO user.
 	InUse bool `json:"inUse"`
+
+	Wallets struct {
+		External int `json:"external"`
+		InApp    int `json:"inApp"`
+	}
 }

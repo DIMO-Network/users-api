7deff1adc5c502f7a3544a13ec34ec1233c548d4
Dylan Moreland <dylan@dimo.zone>
2024-11-01 15:25:08 -0400

    'clear' is from the query parameters

--- internal/controllers/user_controller.go
+++ internal/controllers/user_controller.go
@@ -635,7 +635,7 @@ func (d *UserController) SetMigrated(c *fiber.Ctx) error {
 		return errorResponseHandler(c, err, fiber.StatusInternalServerError)
 	}
 
-	if d.Settings.Environment == "dev" && c.Params("clear") == "true" {
+	if d.Settings.Environment == "dev" && c.Query("clear") == "true" {
 		user.MigratedAt = null.TimeFromPtr(nil)
 	} else {
 		user.MigratedAt = null.TimeFrom(time.Now())
